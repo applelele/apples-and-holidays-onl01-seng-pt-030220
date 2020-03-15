231d145b9f1177df2ee92e4cc540e7ffae0bc832
YUKIKO IKEDA <applelele@gmail.com>
2020-03-15 04:04:32 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -83,6 +83,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.each do |season, data|
     data.each do |event, supplies|
+      binding.pry
       array_event = []
       supplies.each do |item|
        if item == "BBQ"
